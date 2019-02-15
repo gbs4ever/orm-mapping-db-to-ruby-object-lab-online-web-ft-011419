6aca29f7b4eaebfb8f0a1fb2add652ae3458ff36
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-15 20:09:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ class Student
       sql = <<-SQL
           SELECT *
           FROM students
-          WHERE grade = 9;
+          WHERE grade > 12;
           SQL
           DB[:conn].execute(sql).map do |row|
             self.new_from_db(row)

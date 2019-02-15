9367fa6c3e1ebb38ea5183cfaddf2a23efd2a0b0
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-15 20:17:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,6 +66,28 @@ class Student
       end.first
     end
 
+    def self.first_X_students_in_grade_10(number)
+      sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10;
+      SQL
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+    end
+
+    def self.all_students_in_grade_X(grade)
+      sql = <<-SQL
+          SELECT *
+          FROM students
+          WHERE grade = ?
+          SQL
+
+        DB[:conn].execute(sql, grade).map do |row|
+          self.new_from_db(row)
+        end
+    end
 
   def save
     sql = <<-SQL

e85884dc9e0e5e2ae551cde4ea0168b6e94eb3bc
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-15 20:17:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,13 +66,15 @@ class Student
       end.first
     end
 
-    def self.first_X_students_in_grade_10(number)
+    def self.first_X_students_in_grade_10(num)
       sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10;
+      WHERE grade = 10
+      ORDER BY students.id
+      LIMIT ?;
       SQL
-      DB[:conn].execute(sql).map do |row|
+      DB[:conn].execute(sql,num).map do |row|
         self.new_from_db(row)
       end
     end
